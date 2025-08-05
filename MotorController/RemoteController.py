2d522fb6fc8779e2fe77d222a154d3e4438c9f72
NOWorNaver <dydghks5489@naver.com>
2025-08-05 14:50:40 +0900

    qwdwqdqwd

--- MotorController/RemoteController.py
+++ MotorController/RemoteController.py
@@ -5,6 +5,8 @@ SERIAL_PORT = 'COM3'
 SERIAL_BAUD = 115200
 ser = serial.Serial(SERIAL_PORT, SERIAL_BAUD, timeout=1)
 Steering_angle = 0
+state = "STOP"
+
 print("아무 키나 눌러보세요. ESC 키를 누르면 종료됩니다.")
 while True:
 
@@ -18,18 +20,18 @@ while True:
         Steering_angle = 0
     elif(Steering_angle >21) :
         Steering_angle = 0 
-    if (key == 'w') :
+    if (key == b'w') :
         state = "FORWARD"
-    elif(key == 'a') :
+    elif(key == b'a') :
         state = "TURNLEFT"
         Steering_angle = Steering_angle-1
     
 
-    elif (key == 'd') :
+    elif (key == b'w') :
         state ="TURNRIGHT"
         Steering_angle=Steering_angle+1
 
-    elif(key == 's') :
+    elif(key == b'w') :
         state = "REVERSING"
 
 
